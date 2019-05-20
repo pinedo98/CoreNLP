7d1fc90f6f55256aeaa1e54d16a57872c7bed029
Fernando Pinedo <fernando.pinedo@outlook.com>
2019-05-20 18:02:27 -0500

    Rename variable

--- examples/sample-maven-project/src/main/java/edu/stanford/nlp/StanfordCoreNLPSpanishTestApp.java
+++ examples/sample-maven-project/src/main/java/edu/stanford/nlp/StanfordCoreNLPSpanishTestApp.java
@@ -12,9 +12,9 @@ public class StanfordCoreNLPSpanishTestApp
 {
     public static void main(String[] args) throws IOException, ClassNotFoundException
     {
-        String[] chineseArgs = new String[]{"-props","StanfordCoreNLP-spanish.properties",
+        String[] spanishArgs = new String[]{"-props","StanfordCoreNLP-spanish.properties",
                                               "-annotators","tokenize,ssplit,pos,lemma,ner,parse,depparse,kbp",
                                               "-file", "sample-spanish.txt", "-outputFormat", "text"};
-        StanfordCoreNLP.main(chineseArgs);
+        StanfordCoreNLP.main(spanishArgs);
     }
 }
